0141db86241a3fedacfc54aecdc2893d5f1366b0
alexdolg1967 <alexdolg1967@mail.ru>
2022-03-17 17:39:22 +0300

    adaptiv & burger

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -2,8 +2,7 @@
 // Рекомендуется создавать отдельный файл в папке components и подключать все там
 
 // Определение операционной системы на мобильных
-import { mobileCheck } from "./functions/mobile-check";
-console.log(mobileCheck())
+//import { mobileCheck } from "./functions/mobile-check";
 
 // Определение ширины экрана
 // import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
@@ -21,7 +20,7 @@ console.log(mobileCheck())
 // import './functions/fix-fullheight';
 
 // Реализация бургер-меню
-// import { burger } from './functions/burger';
+import { burger } from './functions/burger';
 
 // Реализация остановки скролла (не забудьте вызвать функцию)
 // import { disableScroll } from './functions/disable-scroll';
